ece1c62779c55759aaa228ae4600354ab347aa5d
emanuelhardwell <emanuelnoe16@gmail.com>
2023-07-20 22:53:24 -0600

    feat: The useGlobalPipes was configured

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as morgan from 'morgan';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,13 @@ async function bootstrap() {
   app.enableCors();
   app.use(morgan('dev'));
   app.setGlobalPrefix('/api/v1');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   await app.listen(3000);
 }
