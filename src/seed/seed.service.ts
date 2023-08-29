785f6dc6f916f646da18ced284f04a12505a4851
emanuelhardwell <emanuelnoe16@gmail.com>
2023-08-28 22:17:54 -0600

    feat: The method insertNewProducts of seedService was created

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,13 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { ProductsService } from '../products/products.service';
+import { initialData } from './data/seed-data';
 
 @Injectable()
 export class SeedService {
   constructor(private readonly productsService: ProductsService) {}
 
-  generateSeed() {
-    return `This action returns all seed`;
+  async generateSeed() {
+    await this.insertNewProducts();
+
+    return 'SEED EXECUTED';
   }
 
-  async deleteAllProducts() {}
+  private async insertNewProducts() {
+    await this.productsService.removeAll();
+
+    const arrayProductsSeed = initialData.products;
+
+    const productsSeedPromises = [];
+
+    arrayProductsSeed.forEach((product) => {
+      productsSeedPromises.push(this.productsService.create(product));
+    });
+
+    await Promise.all(productsSeedPromises);
+    return true;
+  }
 }
