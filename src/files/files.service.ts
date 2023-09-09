00fe6f39939eb3b89d48a7a933dc52ed1794fd07
emanuelhardwell <emanuelnoe16@gmail.com>
2023-09-08 22:25:57 -0600

    feat: The method getOneImage of export class FilesService {

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,7 +1,19 @@
-import { Injectable } from '@nestjs/common';
-
+import { Get, Injectable, NotFoundException } from '@nestjs/common';
+import { existsSync } from 'fs';
+import { join } from 'path';
 
 @Injectable()
 export class FilesService {
-  
+  @Get()
+  getOneImage(imageName: string) {
+    const path = join(__dirname, '../../static/products', imageName);
+
+    if (!existsSync(path)) {
+      throw new NotFoundException(
+        `The image with name ${imageName} not exists`,
+      );
+    }
+
+    return path;
+  }
 }
