14abfc70360dec8739a9c576439b1c5c5b5bb5a5
emanuelhardwell <emanuelnoe16@gmail.com>
2023-09-20 22:54:27 -0600

    Fix: The property email of UserEntity was updated

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -1,4 +1,10 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity('users')
 export class User {
@@ -30,4 +36,10 @@ export class User {
     default: ['user'],
   })
   roles: string[];
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  emailBefore() {
+    this.email = this.email.toLowerCase().trim();
+  }
 }
