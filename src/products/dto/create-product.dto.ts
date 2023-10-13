e414e3f329d3426938baf29c00fd94be0e70d7e3
emanuelhardwell <emanuelnoe16@gmail.com>
2023-10-12 23:22:02 -0600

    feat: The decorators ApiProperty and ApiPropertyOptional were added to CreateProductDto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import {
   IsArray,
   IsIn,
@@ -10,28 +11,63 @@ import {
 } from 'class-validator';
 
 export class CreateProductDto {
+  @ApiProperty({
+    description: 'Product title',
+    example: 'Tesla m4 car',
+    // minimum: 1,
+    // default: 1,
+    // type: Number, // No es necesario ponerlo ya que lo infiere
+    // required: true, // No es necesario ya que por defecto lo es
+    nullable: false,
+    minLength: 1,
+  })
   @IsString()
   @MinLength(1)
   title: string;
 
+  @ApiPropertyOptional({
+    description: 'Product price $',
+    example: 35,
+    default: 0,
+  })
   @IsNumber()
   @IsPositive()
   @IsOptional()
   price?: number;
 
+  @ApiPropertyOptional({
+    description: 'Product description',
+    example:
+      'The Relaxed T Logo Hat is a classic silhouette combined with modern ...',
+  })
   @IsString()
   @IsOptional()
   description?: string;
 
+  @ApiPropertyOptional({
+    description: 'Product slug',
+    example: 'thermal_cuffed_beanie',
+  })
   @IsString()
   @IsOptional()
   slug?: string;
 
+  @ApiPropertyOptional({
+    description: 'Product stock',
+    example: 40,
+    default: 0,
+  })
   @IsInt()
   @IsPositive()
   @IsOptional()
   stock?: number;
 
+  @ApiPropertyOptional({
+    description: 'Product sizes',
+    example: ['XS', 'S', 'M', 'L', 'XL', 'XXL'],
+    type: [String],
+    // isArray: true
+  })
   @IsString({ each: true })
   @IsArray()
   sizes: string[];
